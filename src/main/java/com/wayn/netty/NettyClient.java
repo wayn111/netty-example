fa99084159c7a6c0874af2f6b75c74147c08984b
wayn <1669738430@qq.com>
2021-12-26 21:44:10 +0800

    netty实战优化

--- src/main/java/com/wayn/netty/NettyClient.java
+++ src/main/java/com/wayn/netty/NettyClient.java
@@ -2,19 +2,31 @@ package com.wayn.netty;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.CharsetUtil;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class NettyClient {
+    // 心跳发送包，使用unreleasableBuffer避免重复创建对象
+    public static final ByteBuf HEART_BUF = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer(new char[]{'p', 'i', 'n', 'g'}, CharsetUtil.UTF_8));
+
     public static void main(String[] args) {
-        Bootstrap bootstrap = new Bootstrap();
+        final Bootstrap bootstrap = new Bootstrap();
         NioEventLoopGroup group = new NioEventLoopGroup();
 
         bootstrap.group(group)
@@ -22,31 +34,71 @@ public class NettyClient {
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel ch) {
+                        ch.pipeline().addLast("idleStateHandler", new IdleStateHandler(0, 3, 0));
+                        ch.pipeline().addLast(new ChannelDuplexHandler() {
+                            @Override
+                            public void channelInactive(ChannelHandlerContext ctx) {
+                                ctx.executor().schedule(() -> NettyClient.connect(bootstrap), 5, TimeUnit.SECONDS);
+                            }
+
+                            @Override
+                            public void userEventTriggered(ChannelHandlerContext ctx, Object evt) {
+                                if (evt instanceof IdleStateEvent) {
+                                    IdleStateEvent e = (IdleStateEvent) evt;
+                                    if (e.state() == IdleState.WRITER_IDLE) {
+                                        ctx.channel().writeAndFlush(HEART_BUF);
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+                                log.error(cause.getMessage(), cause);
+                                ctx.close();
+                            }
+                        });
+                        ch.pipeline().addLast(new LoggingHandler(LogLevel.INFO));
                         ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                             @Override
                             public void channelActive(ChannelHandlerContext ctx) {
                                 ByteBuf buffer = ctx.alloc().buffer();
-                                buffer.writeBytes((new Date() + ": hello world!#").getBytes(StandardCharsets.UTF_8));
+                                buffer.writeBytes((new Date() + ": hello world!\\r\\n").getBytes(StandardCharsets.UTF_8));
                                 ctx.writeAndFlush(buffer);
                             }
 
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) {
                                 ByteBuf byteBuf = (ByteBuf) msg;
-                                System.out.println(decode(byteBuf));
+                                try {
+                                    log.info(new String(ByteBufUtil.getBytes(byteBuf)));
+                                } finally { // 由于使用的是ChannelInboundHandlerAdapter，需要手动释放byteBuf
+                                    byteBuf.release();
+                                }
                             }
 
-                            private String decode(ByteBuf byteBuf) {
-                                int readableBytes = byteBuf.readableBytes();
-                                byte[] bytes = new byte[readableBytes];
-                                byteBuf.getBytes(byteBuf.readerIndex(), bytes);
-                                return new String(bytes);
-                            }
                         });
                     }
                 });
 
-        bootstrap.connect("127.0.0.1", 99).channel();
+        connect(bootstrap);
 
+    }
+
+    /**
+     * 连接重试
+     *
+     * @param bootstrap
+     */
+    @SneakyThrows
+    private static void connect(Bootstrap bootstrap) {
+        bootstrap.connect("127.0.0.1", 99).sync().addListener(future -> {
+            if (future.isSuccess()) {
+                log.info("连接成功!");
+            } else {
+                Thread.sleep(2000);
+                log.info("连接失败，开始重连");
+                connect(bootstrap);
+            }
+        });
     }
 }
--- src/main/java/com/wayn/netty/NettyServer.java
+++ src/main/java/com/wayn/netty/NettyServer.java
@@ -3,47 +3,116 @@ package com.wayn.netty;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class NettyServer {
 
-    public static void main(String[] args) {
+    private static final int port = 99;
+
+    private static final Map<String, Object> channelMap = new HashMap<>();
+
+
+    public static void main(String[] args) throws InterruptedException {
         ServerBootstrap serverBootstrap = new ServerBootstrap();
+        NioEventLoopGroup boss = null;
+        NioEventLoopGroup worker = null;
+        try {
+            boss = new NioEventLoopGroup();
+            worker = new NioEventLoopGroup();
+            serverBootstrap.group(boss, worker);
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
+                protected void initChannel(NioSocketChannel ch) {
+                    IdleStateHandler idleStateHandler = new IdleStateHandler(5, 0, 0) {
 
-        NioEventLoopGroup boss = new NioEventLoopGroup();
-        NioEventLoopGroup worker = new NioEventLoopGroup();
-        serverBootstrap.group(boss, worker);
-        serverBootstrap.channel(NioServerSocketChannel.class);
-        serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
-            protected void initChannel(NioSocketChannel ch) {
-                ByteBuf delimiter = Unpooled.copiedBuffer("#".getBytes());
-                ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
-                ch.pipeline().addLast("decoder", new StringDecoder(CharsetUtil.UTF_8));
+                        // 心跳检测
+                        @Override
+                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) {
+                            if (evt instanceof IdleStateEvent) {
+                                IdleStateEvent e = (IdleStateEvent) evt;
+                                if (e.state() == IdleState.READER_IDLE) {
+                                    ByteBuf buffer = Unpooled.buffer();
+                                    buffer.writeBytes("pong".getBytes(StandardCharsets.UTF_8));
+                                    ctx.channel().writeAndFlush(buffer).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+                                    String id = ctx.channel().id().asLongText();
+                                    channelMap.forEach((s, o) -> {
+                                        if (s.equals(id)) {
+                                            channelMap.remove(s);
+                                        }
+                                    });
+                                }
+                            }
+                        }
 
-                ch.pipeline().addLast(new SimpleChannelInboundHandler<String>() {
-                    @Override
-                    protected void channelRead0(ChannelHandlerContext ctx, String s) throws Exception {
-                        System.out.println(s);
-                        ctx.writeAndFlush("server");
-                    }
-                });
-                ch.pipeline().addLast("encoder", new StringEncoder(CharsetUtil.UTF_8));
+                        // 发生异常时，关闭连接
+                        @Override
+                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+                            log.error(cause.getMessage(), cause);
+                            String id = ctx.channel().id().asLongText();
+                            channelMap.remove(id);
+                            ctx.close();
+                        }
 
+                    };
+                    ch.pipeline().addLast("idleStateHandler", idleStateHandler);
+                    // 换行符解码器
+                    ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                    ch.pipeline().addLast(new LoggingHandler(LogLevel.INFO));
+                    ch.pipeline().addLast("decoder", new StringDecoder(CharsetUtil.UTF_8));
+                    ch.pipeline().addLast("MyHandle", new SimpleChannelInboundHandler<String>() {
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            String id = ctx.channel().id().asLongText();
+                            log.info("客户端建立连接：{}", id);
+                            channelMap.put(id, ctx.channel());
+                            ctx.channel().writeAndFlush(id);
+                            super.channelActive(ctx);
+                        }
+
+                        @Override
+                        protected void channelRead0(ChannelHandlerContext ctx, String s) {
+                            log.info("收到客户端消息：{}", s);
+                            ctx.channel().writeAndFlush("server");
+                        }
+
+                    });
+                    ch.pipeline().addLast("encoder", new StringEncoder(CharsetUtil.UTF_8));
+
+                }
+            });
+            ChannelFuture channelFuture = serverBootstrap.bind(port).sync().addListener(future -> {
+                if (future.isSuccess()) {
+                    log.info("server start up on {}", port);
+                }
+            });
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            if (boss != null) {
+                boss.shutdownGracefully();
             }
-        });
-        serverBootstrap.bind(99);
+            if (worker != null) {
+                worker.shutdownGracefully();
+            }
+        }
     }
 
-
 }
+
