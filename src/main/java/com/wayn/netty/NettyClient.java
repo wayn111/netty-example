caa0c63180f725ae0aa47ab60e5d8f8076e89ad8
wayn <1669738430@qq.com>
2022-01-03 21:18:17 +0800

    netty实战优化

--- src/main/java/com/wayn/netty/NettyClient.java
+++ src/main/java/com/wayn/netty/NettyClient.java
@@ -13,20 +13,23 @@ import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import io.netty.handler.timeout.IdleStateHandler;
 import io.netty.util.CharsetUtil;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 带有连接重试的tcp客户端
+ */
 @Slf4j
 public class NettyClient {
     // 心跳发送包，使用unreleasableBuffer避免重复创建对象
-    public static final ByteBuf HEART_BUF = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer(new char[]{'p', 'i', 'n', 'g'}, CharsetUtil.UTF_8));
+    public static final ByteBuf HEART_BUF = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("ping", CharsetUtil.UTF_8));
 
-    public static void main(String[] args) {
-        final Bootstrap bootstrap = new Bootstrap();
+    public static void main(String[] args) throws InterruptedException {
+        // 创建客户端启动器
+        Bootstrap bootstrap = new Bootstrap();
         NioEventLoopGroup group = new NioEventLoopGroup();
 
         bootstrap.group(group)
@@ -34,13 +37,17 @@ public class NettyClient {
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel ch) {
+                        // 创建一个心跳检测的handle，客户端创建连接后，3秒内没有收到服务端的心跳就会触发IdleState.WRITER_IDLE
                         ch.pipeline().addLast("idleStateHandler", new IdleStateHandler(0, 3, 0));
+                        // 使用双向处理器，发送ping消息给服务端
                         ch.pipeline().addLast(new ChannelDuplexHandler() {
+                            // 服务端连接关闭时，进行重试操作
                             @Override
                             public void channelInactive(ChannelHandlerContext ctx) {
                                 ctx.executor().schedule(() -> NettyClient.connect(bootstrap), 5, TimeUnit.SECONDS);
                             }
 
+                            // 3秒内没有向服务端发送心跳会触发userEventTriggered方法
                             @Override
                             public void userEventTriggered(ChannelHandlerContext ctx, Object evt) {
                                 if (evt instanceof IdleStateEvent) {
@@ -51,14 +58,19 @@ public class NettyClient {
                                 }
                             }
 
+                            // 发生异常时，关闭连接
                             @Override
                             public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
                                 log.error(cause.getMessage(), cause);
                                 ctx.close();
                             }
                         });
+                        // netty日志记录，打印包信息
                         ch.pipeline().addLast(new LoggingHandler(LogLevel.INFO));
+                        // 自定义解码器，实现自定义业务逻辑，使用ChannelInboundHandlerAdapter时需要手动关闭byteBuf
                         ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+
+                            // 连接建立触发channelActive
                             @Override
                             public void channelActive(ChannelHandlerContext ctx) {
                                 ByteBuf buffer = ctx.alloc().buffer();
@@ -66,6 +78,7 @@ public class NettyClient {
                                 ctx.writeAndFlush(buffer);
                             }
 
+                            // 收到来自服务端的消息
                             @Override
                             public void channelRead(ChannelHandlerContext ctx, Object msg) {
                                 ByteBuf byteBuf = (ByteBuf) msg;
@@ -89,16 +102,20 @@ public class NettyClient {
      *
      * @param bootstrap
      */
-    @SneakyThrows
     private static void connect(Bootstrap bootstrap) {
-        bootstrap.connect("127.0.0.1", 99).sync().addListener(future -> {
-            if (future.isSuccess()) {
-                log.info("连接成功!");
-            } else {
-                Thread.sleep(2000);
-                log.info("连接失败，开始重连");
-                connect(bootstrap);
-            }
-        });
+        try {
+            bootstrap.connect("127.0.0.1", 99).sync().addListener(future -> {
+                if (future.isSuccess()) {
+                    log.info("连接成功!");
+                } else {
+                    Thread.sleep(2000);
+                    log.info("连接失败，开始重连");
+                    connect(bootstrap);
+                }
+            });
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            connect(bootstrap);
+        }
     }
 }
--- src/main/java/com/wayn/netty/NettyServer.java
+++ src/main/java/com/wayn/netty/NettyServer.java
@@ -22,37 +22,46 @@ import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * tcp服务端
+ */
 @Slf4j
 public class NettyServer {
 
+    // 绑定端口
     private static final int port = 99;
 
+    // 维护channel列表，将用户信息和channel做绑定，一个channel代表一个tcp连接
     private static final Map<String, Object> channelMap = new HashMap<>();
 
-
     public static void main(String[] args) throws InterruptedException {
+        // 创建服务端启动器
         ServerBootstrap serverBootstrap = new ServerBootstrap();
+        // 指定连接接收线程，处理所有用户连接，转给工作线程
         NioEventLoopGroup boss = null;
+        // 指定工作线程，处理用户连接
         NioEventLoopGroup worker = null;
         try {
             boss = new NioEventLoopGroup();
             worker = new NioEventLoopGroup();
-            serverBootstrap.group(boss, worker);
-            serverBootstrap.channel(NioServerSocketChannel.class);
+            serverBootstrap.group(boss, worker); // 绑定线程组
+            serverBootstrap.channel(NioServerSocketChannel.class);  // 底层使用nio处理
             serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
                 protected void initChannel(NioSocketChannel ch) {
+                    // 创建一个心跳检测的handle，服务端创建连接后，5秒内没有收到客户端的心跳就会触发IdleState.READER_IDLE
                     IdleStateHandler idleStateHandler = new IdleStateHandler(5, 0, 0) {
-
-                        // 心跳检测
+                        // 5秒内没有收到客户端的心跳会触发userEventTriggered方法
                         @Override
                         public void userEventTriggered(ChannelHandlerContext ctx, Object evt) {
                             if (evt instanceof IdleStateEvent) {
                                 IdleStateEvent e = (IdleStateEvent) evt;
+                                // 检测是读状态就发送pong消息
                                 if (e.state() == IdleState.READER_IDLE) {
                                     ByteBuf buffer = Unpooled.buffer();
                                     buffer.writeBytes("pong".getBytes(StandardCharsets.UTF_8));
                                     ctx.channel().writeAndFlush(buffer).addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
                                     String id = ctx.channel().id().asLongText();
+                                    // 删除channelMap中客户端连接
                                     channelMap.forEach((s, o) -> {
                                         if (s.equals(id)) {
                                             channelMap.remove(s);
@@ -72,30 +81,36 @@ public class NettyServer {
                         }
 
                     };
+                    // 通过对象实例，而不是new创建，避免大量客户端连接导致浪费空间
                     ch.pipeline().addLast("idleStateHandler", idleStateHandler);
-                    // 换行符解码器
+                    // 换行符解码器，指定包与包之间的分隔符是"\n"和"\r\n",避免拆包粘包
                     ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                    // netty日志记录，打印包信息
                     ch.pipeline().addLast(new LoggingHandler(LogLevel.INFO));
+                    // 入站字符解码器
                     ch.pipeline().addLast("decoder", new StringDecoder(CharsetUtil.UTF_8));
+                    // 自定义解码器，实现自定义业务逻辑，使用SimpleChannelInboundHandler可以避免ByteBuf的回收问题
                     ch.pipeline().addLast("MyHandle", new SimpleChannelInboundHandler<String>() {
+                        // 连接第一次创建会调用这个方法
                         @Override
                         public void channelActive(ChannelHandlerContext ctx) throws Exception {
                             String id = ctx.channel().id().asLongText();
                             log.info("客户端建立连接：{}", id);
                             channelMap.put(id, ctx.channel());
-                            ctx.channel().writeAndFlush(id);
+                            ctx.channel().writeAndFlush("this is come from server!");
                             super.channelActive(ctx);
                         }
 
+                        // 获取来自客户端的数据
                         @Override
                         protected void channelRead0(ChannelHandlerContext ctx, String s) {
                             log.info("收到客户端消息：{}", s);
-                            ctx.channel().writeAndFlush("server");
+                            ctx.channel().writeAndFlush("server reply\r\n");
                         }
 
                     });
+                    // 出站字符编码器
                     ch.pipeline().addLast("encoder", new StringEncoder(CharsetUtil.UTF_8));
-
                 }
             });
             ChannelFuture channelFuture = serverBootstrap.bind(port).sync().addListener(future -> {
@@ -105,6 +120,7 @@ public class NettyServer {
             });
             channelFuture.channel().closeFuture().sync();
         } finally {
+            // 不要忘了在finally中关闭线程组
             if (boss != null) {
                 boss.shutdownGracefully();
             }
